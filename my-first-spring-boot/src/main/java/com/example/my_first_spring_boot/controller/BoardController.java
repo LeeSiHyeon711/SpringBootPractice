21e5e4ac0e53e198e34b2804bb5744f0f234aec5
LeeSiHyeon711 <dltlgus1230@naver.com>
2024-10-01 22:40:33 +0900

    게시글작성기능추가

--- my-first-spring-boot/src/main/java/com/example/my_first_spring_boot/controller/BoardController.java
+++ my-first-spring-boot/src/main/java/com/example/my_first_spring_boot/controller/BoardController.java
@@ -1,30 +1,51 @@
 package com.example.my_first_spring_boot.controller;
 
+import com.example.my_first_spring_boot.repository.BoardRepository;
 import org.springframework.ui.Model;
 import com.example.my_first_spring_boot.entity.BoardEntity;
 import com.example.my_first_spring_boot.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class BoardController {
     @Autowired
-    private BoardService boardService;
+    private BoardService boardService;//서비스 가져오기 및 자동 주입
+    @Autowired
+    private BoardRepository boardRepository;//레파지토리 가져오기 및 자동 주입
+    //게시판 글 목록 컨트롤러
     @GetMapping("/boardList")
     public String boardList(Model model) {
         List<BoardEntity> boardEntities = boardService.findAllBoards();
+        long totalCount = boardService.totalBoards();
         model.addAttribute("boardEntities", boardEntities);
+        model.addAttribute("totalCount", totalCount);
         return "boardList";
     }
+    //게시글 상세보기 컨트롤러
     @GetMapping("/viewPost/{id}")
     public String viewPost(@PathVariable long id, Model model) {
         BoardEntity boardEntity = boardService.findBoardById(id);
         boardService.increaseViews(boardEntity);//조회수 증가
         model.addAttribute("boardEntity", boardEntity);
         return "viewPost";
+    }
+    @GetMapping("/addBoardForm")
+    //게시글 작성 폼 보여주기 컨트롤러
+    public String showAddBoardForm(Model model) {
+        model.addAttribute("boardEntity", new BoardEntity());
+        return "addBoardForm";
+    }
+    //게시글 등록 정보 보내기 컨트롤러
+    @PostMapping("/addBoard")
+    public String addBoard(@ModelAttribute BoardEntity boardEntity) {
+        boardService.saveBoard(boardEntity);
+        return "redirect:/boardList";
     }
 }
--- my-first-spring-boot/src/main/java/com/example/my_first_spring_boot/service/BoardService.java
+++ my-first-spring-boot/src/main/java/com/example/my_first_spring_boot/service/BoardService.java
@@ -11,14 +11,23 @@ import java.util.List;
 public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
+    //게시글 목록을 불러오는 서비스
     public List<BoardEntity> findAllBoards() {
         return boardRepository.findAll();
     }
+    //게시글 총 갯수를 불러오는 서비스
+    public long totalBoards() { return boardRepository.count(); }
+    //id에 해당하는 게시글을 불러오는 서비스
     public BoardEntity findBoardById(Long id) {
         return boardRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Invalid Board Id:"+id));
     }
+    //게시글 조회시 조회수 증가 서비스
     public void increaseViews(BoardEntity boardEntity) {
         boardEntity.setViews(boardEntity.getViews() + 1);
+        boardRepository.save(boardEntity);
+    }
+    //게시글 등록 서비스
+    public void saveBoard(BoardEntity boardEntity) {
         boardRepository.save(boardEntity);
     }
 }
